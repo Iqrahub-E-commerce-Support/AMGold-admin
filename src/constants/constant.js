231c3e7544cda1eb1b8db60054952525eb5fb2e4
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-11-30 13:47:52 +0530

    block and unblock functionality dne

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,2 +1,2 @@
-// export const  baseURL = 'https://amgold-server.onrender.com'
-export const  baseURL = 'http://localhost:8000'
+export const  baseURL = 'https://amgold-server.onrender.com'
+// export const  baseURL = 'http://localhost:8000'
