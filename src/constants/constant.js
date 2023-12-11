ac7e2ad1488da3266e415ae61aedf9151306258f
abrar aboobacker <abraraboobackerkk@gmail.com>
2023-12-11 10:45:06 +0530

    url changed

--- src/constants/constant.js
+++ src/constants/constant.js
@@ -1,2 +1,2 @@
-export const  baseURL = 'https://amgold-server.onrender.com'
+export const  baseURL = 'https://appserver.almuqtadirgoldwholesale.com'
 // export const  baseURL = 'http://localhost:8000'
